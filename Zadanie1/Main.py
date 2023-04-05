5bc7d17f0f0ee6ac243e77bac73a0f3db7c6ba7d
l.sudol <l.sudol@wasko.pl>
2023-04-05 13:49:11 +0200

    Spróbowałem inaczej, plan był chyba dobry ale błędy wyskakują nonstop

--- Zadanie1/Main.py
+++ Zadanie1/Main.py
@@ -1,12 +1,37 @@
 digits = []
-digit = input("Podaj cyfrę aby zakończyć wpisz 'k'")
-print(digits)
+input_list = input("Podaj cyfry oddzielone spacją")
+digits = input_list.split()
+for digit in digits:
+    digit = digit.replace(",",".")
+    try:
+        digits.append(float(digit))
+    except ValueError:
+        pass
+    if len(digits) == 0:
+        return 0
+    return avg = sum(digits)/len(digits)
+print(avg)
 
-while digit!="k":
-    if digit.isnumeric() :
-        digits.append(digit)
-        digit = input("Podaj cyfrę aby zakończyć wpisz 'k'")
+
+
+
+'''
+    if digit.isnumeric() : #sprawdza czy jest cyfrą
+        digits.append(float(digit))
+        digit = input("Podaj cyfrę aby zako`+ńczyć wpisz 'enter'")
         print(digit)
-
-
+        
+    else :                  #ignoruje jeśli nie jest cyfrą
+        digit = input("Podaj cyfrę aby zakończyć wpisz 'enter'")
 print(digits)
+
+summary = sum(digits)
+print(summary)
+'''
+
+#avg = summary/len(digits)
+#print(avg)
+#jesli w stringu znajdzie . albo , to zapisuje go jako float
+#i wczesniej zmienic , na . replace
+#  digits.append(float(digit))
+#try !!!
