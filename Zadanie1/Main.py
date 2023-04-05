d1f4dfd3aa242bf5ce229493977c1c42bd3da2d3
l.sudol <l.sudol@wasko.pl>
2023-04-05 14:38:53 +0200

    Zadanie zrobione, wystarczyło dodać listę jeszcze jedną

--- Zadanie1/Main.py
+++ Zadanie1/Main.py
@@ -1,21 +1,21 @@
 digits = []
+end_list = [] #lista ostateczna, z przekonwertowanymi wartosciami
 input_list = input("Podaj cyfry oddzielone spacją")
-digits = input_list.split()
+digits = input_list.split() #dziele liste spacjami i zapisuje ja w nowej liscie
 for digit in digits:
-    digit = digit.replace(",",".")
-    try:
-        digits.append(float(digit))
+    digit = digit.replace(",",".") #zamieniam przecinki na kropki, żeby złapał float
+    try: #próbuje zmienic na float, jesli sie nie udaje czyli jest litera lub inny znak to ignoruje
+        end_list.append(float(digit))
     except ValueError:
         pass
-    if len(digits) == 0:
-        return 0
-    return avg = sum(digits)/len(digits)
+    if len(digits) == 0: #wyjatek 0/0
+        avg = 0
+        print(avg)
+avg = sum(end_list)/len(end_list)
 print(avg)
 
 
-
-
-'''
+''' moje notatki
     if digit.isnumeric() : #sprawdza czy jest cyfrą
         digits.append(float(digit))
         digit = input("Podaj cyfrę aby zako`+ńczyć wpisz 'enter'")
