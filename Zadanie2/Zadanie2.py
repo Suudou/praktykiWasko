34b7039520d58a8a7824b204e514490055da5b45
l.sudol <l.sudol@wasko.pl>
2023-04-19 15:47:47 +0200

    Dodałem string.punctuation

--- Zadanie2/Zadanie2.py
+++ Zadanie2/Zadanie2.py
@@ -1,20 +1,20 @@
 import string
+if __name__ == "__main__":
+
+    def convert(lst):
+        keys = lst
+        dictionar = {key: 0 for key in keys}
+        return dictionar
 
 
-def convert(lst):
-    keys = lst
-    dictionar = {key: 0 for key in keys}
-    return dictionar
-
-
-sentence = input("podaj ciąg znaków \n ")
-x = string.ascii_lowercase
-dic = list(string.ascii_lowercase + string.ascii_uppercase + string.digits)
-dictionary = convert(dic)
-for element in sentence:
-    if element in dictionary:
-        dictionary[element] += 1
-for key, value in dictionary.items():
-    print(key, "-", value, " \n ")
+    sentence = input("podaj ciąg znaków \n ")
+    x = string.ascii_lowercase
+    dic = list(string.ascii_lowercase + string.ascii_uppercase + string.digits + string.punctuation)
+    dictionary = convert(dic)
+    for element in sentence:
+        if element in dictionary:
+            dictionary[element] += 1
+    for key, value in dictionary.items():
+        print(key, "-", value, " \n ")
 
 
