51781dfdd093cc154bfea27110511c08fd605744
l.sudol <l.sudol@wasko.pl>
2023-04-19 15:27:47 +0200

    Poprawione komentarze, do poprawy jeszcze błąd przy braku parametrów

--- Zadanie1/Main.py
+++ Zadanie1/Main.py
@@ -1,15 +1,19 @@
 if __name__ == "__main__":
     end_list = []
-    #lista ostateczna, z przekonwertowanymi wartosciami
+    # lista ostateczna, z przekonwertowanymi wartosciami
     input_list = input("Podaj cyfry oddzielone spacją")
-    digits = input_list.split() #dziele liste spacjami i zapisuje ja w nowej liscie
+    # dziele liste spacjami i zapisuje ja w nowej liscie
+    digits = input_list.split()
     for digit in digits:
-        digit = digit.replace(",", ".") #zamieniam przecinki na kropki, żeby złapał float
-        try: #próbuje zmienic na float, jesli sie nie udaje czyli jest litera lub inny znak to ignoruje
+        # zamieniam przecinki na kropki, żeby złapał float
+        digit = digit.replace(",", ".")
+        # próbuje zmienic na float, jesli sie nie udaje czyli jest litera lub inny znak to ignoruje
+        try:
             end_list.append(float(digit))
         except ValueError:
             pass
-        if len(digits) == 0: #wyjatek 0/0
+        # wyjatek 0/0
+        if len(digits) == 0:
             avg = 0
             print(avg)
     avg = sum(end_list)/len(end_list)
