4625f65ad646dbc56bf9abe963b4788c6fd14ba5
l.sudol <l.sudol@wasko.pl>
2023-07-24 11:41:27 +0200

    klasa posiadająca metody wzorcowe dla urli które w klasach dziedziczących będą hardkodowane

--- Projekt/weather/baseWeather.py
+++ Projekt/weather/baseWeather.py
@@ -9,17 +9,14 @@ class URLTemplate:
         self.ALL_CITIES_URL = all_cities_url
         self.SPECIFIC_CITIES_URL = specific_cities_url
 
-        def get_all_cities(all_cities_url: Template, parameters: dict):
-            # url = all_cities_url.substitute(**parameters)
-            url = all_cities_url.substitute(**parameters)
-            response = requests.get(url)
-            return json.loads(response.text)
+    def get_all_cities(all_cities_url: Template, parameters: dict):
+        # url = all_cities_url.substitute(**parameters)
+        url = all_cities_url.substitute(**parameters)
+        response = requests.get(url)
+        return json.loads(response.text)
 
-        def get_specific_cities(specific_cities_url: Template, **kwargs) -> dict:
-            # url = all_cities_url.substitute(**parameters)
-            url = specific_cities_url.substitute(kwargs)
-            response = requests.get(url)
-            return json.loads(response.text)
-
-
-#kontruktor przyjmuje 2 stringi, "url"e all_cities_url:Template
+    def get_specific_cities(specific_cities_url: Template, **kwargs) -> dict:
+        # url = all_cities_url.substitute(**parameters)
+        url = specific_cities_url.substitute(kwargs)
+        response = requests.get(url)
+        return json.loads(response.text)
