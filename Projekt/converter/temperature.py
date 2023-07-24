a3a05afa74b10caa65dc414e730ca988c5f46eb7
l.sudol <l.sudol@wasko.pl>
2023-07-24 11:16:28 +0200

    druga wersja Funkcji konwertującej temperature, rekurencja zastąpiona funkcją zanieżdżoną

--- Projekt/converter/temperature.py
+++ Projekt/converter/temperature.py
@@ -1,26 +1,5 @@
-"""
-def convert_temperature(dst, val, src='C'):
-    if src == dst:
-        return val
-    if src != 'C' and dst != 'C':
-        auxiliary_value = convert_temperature('C', val, src)
-        return convert_temperature(dst, auxiliary_value, 'C')
-    if dst == 'C':
-        match src:
-            case 'F':
-                return (val - 32) * 5/9
-            case 'K':
-                return val - 273
-    elif src == 'C':
-        match dst:
-            case 'F':
-                return (val * 9/5) + 32
-            case 'K':
-                return val + 273
-    else:
-        raise ValueError("Błąd" + src)"""
 
-#druga wersja Funkcji konwertującej temperature, rekurencja zastąpiona funkcją zanieżdżoną
+
 def convert_temperature(dst, val, src='C'):
     def source_to_celsius(value, source):
         match source:
