34e4ca78345ce3fceaaef9794015ec6677b10b82
l.sudol <l.sudol@wasko.pl>
2023-07-25 08:45:19 +0200

    Update URLTemplate class

--- Projekt/weather/baseWeather.py
+++ Projekt/weather/baseWeather.py
@@ -5,18 +5,18 @@ import requests
 
 
 class URLTemplate:
-    def __init__(self, all_cities_url: Template, specific_cities_url: Template):
-        self.ALL_CITIES_URL = all_cities_url
-        self.SPECIFIC_CITIES_URL = specific_cities_url
+    def __init__(self, all_cities_url: str, specific_cities_url: str):
+        self.ALL_CITIES_URL = Template(all_cities_url)
+        self.SPECIFIC_CITIES_URL = Template(specific_cities_url)
 
-    def get_all_cities(all_cities_url: Template, parameters: dict):
+    def get_all_cities(self, all_cities_url: Template, parameters: dict):
         # url = all_cities_url.substitute(**parameters)
-        url = all_cities_url.substitute(**parameters)
+        url = self.ALL_CITIES_URL.substitute(**parameters)
         response = requests.get(url)
         return json.loads(response.text)
 
-    def get_specific_cities(specific_cities_url: Template, **kwargs) -> dict:
+    def get_specific_cities(self, specific_cities_url: Template, **kwargs) -> dict:
         # url = all_cities_url.substitute(**parameters)
-        url = specific_cities_url.substitute(kwargs)
+        url = self.SPECIFIC_CITIES_URL.substitute(kwargs)
         response = requests.get(url)
         return json.loads(response.text)
