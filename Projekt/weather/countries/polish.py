7644394566c8a290c7d0185e433c2ecc6c688cfe
l.sudol <l.sudol@wasko.pl>
2023-07-26 15:44:18 +0200

    Update get_city_names method

--- Projekt/weather/countries/polish.py
+++ Projekt/weather/countries/polish.py
@@ -1,43 +1,22 @@
 from string import Template
 from fastapi import requests
-import sys
-from Projekt.weather import baseWeather
+import json
+
+from Projekt.weather.baseWeather import URLTemplate
 
 POLISH_ALL_CITIES = "https://danepubliczne.imgw.pl/api/data/synop"
 POLISH_SPECIFIC_CITY = "https://danepubliczne.imgw.pl/api/data/synop/station/$cityname"
-parameters = {}
-
-all_polish_cities = baseWeather.URLTemplate.get_all_cities(Template(POLISH_ALL_CITIES), parameters)
-# print([station.get('stacja') for station in all_polish_cities])
 
 
-def city_names_list():
-    city_names_list = []
-    for station in all_polish_cities:
-        city = station.get('stacja')
-        city_names_list.append(city)
-    return city_names_list
+class PolishWeather(URLTemplate):
+    def __init__(self, all_cities_url: str, specific_cities_url: str):
+        super().__init__(all_cities_url, specific_cities_url)
 
+    def get_city_names(self, ) -> list:
+        city_names_list = []
+        all_cities = self._get_all_cities()
+        for station in all_cities:
+            city = station.get('stacja')
+            city_names_list.append(city)
+        return city_names_list
 
-print(city_names_list())
-
-
-"""#2 zmienne od templatów
-# klasie potomnej
-POLISH_ALL_CITIES = "https://danepubliczne.imgw.pl/api/data/synop"
-POLISH_SPECIFIC_CITY = "https://danepubliczne.imgw.pl/api/data/synop/station/$cityname"
-
-# To powinno być rozbite
-# konstruktor klasy bazowej przyjmuje stringa
-# konstruktor klasy potomnej zapewnia tego stringa
-tmp = Template(POLISH_ALL_CITIES)
-tmp2 = Template(POLISH_SPECIFIC_CITY)
-
-print(get_all_cities(tmp, {}))
-print(get_specific_cities(tmp2, cityname='hel'))
-
-
-# klasa potomna - get_all_city_names
-weather_data = get_all_cities(tmp, {}) # json ze wszystkim
-print([station.get('stacja') for station in weather_data])
-"""
