48405516f1006d8ca684e539c518324d942851fb
l.sudol <l.sudol@wasko.pl>
2023-07-31 13:50:34 +0200

    Update get_city_names func

--- Projekt/weather/countries/germany.py
+++ Projekt/weather/countries/germany.py
@@ -1,19 +1,20 @@
 import os
 import pandas as pd
 from Projekt.weather.baseWeather import URLTemplate
+import datetime
+import requests
+
+
+GERMAN_SPECIFIC_CITY = "https://api.brightsky.dev/weather?$cityname"
+GERMAN_ALL_CITIES = "https://brightsky.dev/demo/cities.json"
 
 
 class GermanyWeather(URLTemplate):
+    def __init__(self, all_cities_url: str, specific_cities_url: str):
+        super().__init__(all_cities_url, specific_cities_url)
 
-    def get_city_names(self, ) -> list:
-        file_path = 'C:\\Users\\l.sudol\\PycharmProjects\\lsudol\\Projekt\\weather\\countries\\german_data.xls'
-        print("git") if os.path.exists(file_path) else print("źle")
-        german_data = pd.read_excel(file_path)
-        # Wyciągnięcie jednej kolumny (np. kolumna z nazwami stacji)
-        station_names = german_data['Stations-Name']
-        german_cities_list = station_names.tolist()
-        return german_cities_list
-
-
-
-
+    def get_city_names(self) -> list:
+        # city_names_list = []
+        all_cities = self._get_all_cities()
+        city_names_list = [station['name'] for station in all_cities]
+        return city_names_list
