9466c46ad230f0854c281b168c9abd4f3352ecb7
l.sudol <l.sudol@wasko.pl>
2023-08-09 11:02:29 +0200

    Add units in class weather

--- Projekt/weather/weather.py
+++ Projekt/weather/weather.py
@@ -1,12 +1,22 @@
-from Projekt.weather.countries.polish import PolishWeather
-import Projekt.weather.countries.polish
-
-# get_formatted_city_data wchodzi do weather, i zamieniam słownik w pola
-
 
 class Weather:
-    def __init__(self, **kwargs):
+    def __init__(self, data: dict):
         self.data = {}
-        for key, value in kwargs.items():
-            self.data[key] = value
+        units = {
+            'Temperature': 'C',
+            'Pressure': 'hPa',
+            'Rainfall': 'mm',
+            'Wind velocity': 'km/h',
+            'Humidity': '%',
+            'Hour': 'CEST'
+        }
+        for key, value in data.items():
+            if key in units:
+                self.data[key] = {'value': value, 'unit': units[key]}
+            else:
+                self.data[key] = value
+
+    def __str__(self):
+        return str(self.data)
+
 
