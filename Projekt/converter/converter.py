d1679996adf98ec8628d80e23a9da2ba4d11697e
l.sudol <l.sudol@wasko.pl>
2023-08-10 12:50:58 +0200

    Add Converter Class with static method convert

--- Projekt/converter/converter.py
+++ Projekt/converter/converter.py
@@ -1,4 +1,10 @@
 import temperature
+import WindVelocity
+
+class Converter:
+    def convert(unit_type: str, dst: str, val: float, src: str):
+        match unit_type:
+            case "temperature": return temperature.convert_temperature(dst, val, src)
+            case "wind_velocity": return WindVelocity.convert_velocity(dst, val, src)
 
 
-print(temperature.convert_temperature('K', 30, 'F'))
