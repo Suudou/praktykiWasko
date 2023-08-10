203d6774aa32cf3cb2808722176f0cd8772f3907
l.sudol <l.sudol@wasko.pl>
2023-08-10 15:07:34 +0200

    Changed Wind_velcity name

--- Projekt/converter/converter.py
+++ Projekt/converter/converter.py
@@ -1,10 +1,9 @@
 import temperature
-import WindVelocity
+import Velocity
+
 
 class Converter:
     def convert(unit_type: str, dst: str, val: float, src: str):
         match unit_type:
             case "temperature": return temperature.convert_temperature(dst, val, src)
-            case "wind_velocity": return WindVelocity.convert_velocity(dst, val, src)
-
-
+            case "wind_velocity": return Velocity.convert_velocity(dst, val, src)
