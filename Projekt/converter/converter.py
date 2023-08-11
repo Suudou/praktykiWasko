6e1c195b847f77848f97d16a3fce7246f616e38f
l.sudol <l.sudol@wasko.pl>
2023-08-11 11:28:45 +0200

    imports Fixup

--- Projekt/converter/converter.py
+++ Projekt/converter/converter.py
@@ -1,9 +1,9 @@
-import temperature
-import Velocity
+from .Velocity import convert_velocity
+from .temperature import convert_temperature
 
 
 class Converter:
     def convert(unit_type: str, dst: str, val: float, src: str):
         match unit_type:
-            case "temperature": return temperature.convert_temperature(dst, val, src)
-            case "wind_velocity": return Velocity.convert_velocity(dst, val, src)
+            case "temperature": return convert_temperature(dst, val, src)
+            case "wind_velocity": return convert_velocity(dst, val, src)
